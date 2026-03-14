eb06d54d95d0f447b130638690283dcd0a8cbe63
blanczkju <k.silwon1@gmail.com>
2026-03-14 16:05:00 +0100

    Lab 2 - Uzytkownik, aktywny projekt, historyjki

--- src/api/storageKey.ts
+++ src/api/storageKey.ts
@@ -1,1 +1,3 @@
 export const PROJECTS_STORAGE_KEY = 'manageme_projects'
+export const ACTIVE_PROJECT_STORAGE_KEY = 'manageme_active_project'
+export const HISTORYJKI_STORAGE_KEY = 'manageme_historyjki'
